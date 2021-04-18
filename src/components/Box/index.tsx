036ad8d0031fa564ef20a51c7240b4e041ecac16
bmanth60 <bman.brianthai@gmail.com>
2021-04-18 16:42:41 -0700

    Removed interactions.

--- src/components/Box/index.tsx
+++ src/components/Box/index.tsx
@@ -8,20 +8,20 @@ export default function Box(props: JSX.IntrinsicElements['mesh']) {
     const [active, setActive] = useState(false)
     useFrame((state, _delta) => {
         const time = state.clock.getElapsedTime()
-        mesh.current.rotation.x += 0.01
-        mesh.current.position.y = mesh.current.position.y + Math.sin(time*2)/100;
+        // mesh.current.rotation.x += 0.01
+        // mesh.current.position.y = mesh.current.position.y + Math.sin(time*2)/100;
     })
     return (
         <mesh
             {...props}
             ref={mesh}
-            scale={active ? 1.5 : 1}
+            scale={1}
             onClick={(_event) => setActive(!active)}
             onPointerOver={(_event) => setHover(true)}
             onPointerOut={(_event) => setHover(false)}
         >
             <boxGeometry args={[1, 1, 1]} />
-            <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+            <meshStandardMaterial color={'orange'} />
         </mesh>
     )
 }
